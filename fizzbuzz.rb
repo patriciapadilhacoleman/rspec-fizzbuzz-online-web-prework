4736d4f40eb29a18eb88515db735e486f678cb0c
Patricia Padilha Coleman <patricia.p.coleman@gmail.com>
2019-01-02 19:11:15 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,15 +1,21 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
-  if int % 3 == 0 # if the number int is divisible by 3
-    "Fizz" # Go fizz
+  if (int % 3 == 0) && (int % 5 == 0)
+    
+    "FizzBuzz" # Go fizzBuzz
+  
   elsif int % 5 == 0
+    
     "Buzz" # Go buzz
   
-  elsif (int % 3 == 0) && (int % 5 == 0)
-     "FizzBuzz" # Go FizzBuzz
-  
+  elsif int % 3 == 0
+     
+     "Fizz" # Go Fizz
   else
+    
     nil
+  
   end
+
 end
